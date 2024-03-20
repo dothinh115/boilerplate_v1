ffc1816da759140e58cb441eee6b1c8d3c48b7d5
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-03-21 06:19:43 +0700

    update

--- src/core/mongoose/models/user.model.ts
+++ src/core/mongoose/models/user.model.ts
@@ -2,20 +2,45 @@ import { Prop } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 
 export default class DefaultUser {
-  @Prop({ auto: true })
+  @Prop({ auto: true, input: 'text', disabled: true })
   _id: mongoose.Schema.Types.ObjectId;
-  @Prop({ required: true, lowercase: true, unique: true, trim: true })
+  @Prop({
+    required: true,
+    lowercase: true,
+    unique: true,
+    trim: true,
+    type: mongoose.Schema.Types.String,
+    input: 'text',
+    disabled: true,
+  })
   email: string;
-  @Prop({ required: true, select: false })
+  @Prop({
+    required: true,
+    select: false,
+    type: mongoose.Schema.Types.String,
+    input: 'password',
+  })
   password: string;
-  @Prop({ default: false })
+  @Prop({
+    default: false,
+    type: mongoose.Schema.Types.Boolean,
+    input: 'boolean',
+    disabled: true,
+  })
   actived: boolean;
   @Prop({
     type: mongoose.Schema.Types.String,
     ref: 'Role',
     default: null,
+    input: 'text',
   })
   role: string;
-  @Prop({ default: false, immutable: true })
+  @Prop({
+    default: false,
+    immutable: true,
+    type: mongoose.Schema.Types.Boolean,
+    input: 'boolean',
+    disabled: true,
+  })
   rootUser: boolean;
 }
